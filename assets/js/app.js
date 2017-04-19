6f40721e9fe1e72f9d694f7e3224f0c222282619
Misshel <jenmish094@gmail.com>
2017-04-19 11:25:46 -0500

    creacion de las etiquetas html desde Js

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+
+var body= document.getElementsByTagName("BODY")[0];
+var section= document.createElement("section");
+var cancha=document.createElement("div");
+    cancha.setAttribute("class","cancha");
+
+section.appendChild(cancha);
+body.appendChild(section);
+
+for(var i=0;i<6;i++){
+  var div=document.createElement("div");
+  cancha.appendChild(div);
+  if(i<3){
+    div.setAttribute("class","pelotas");
+  }else if (i>2 & i<5) {
+    div.setAttribute("class","porteria");
+  }else {
+    div.setAttribute("class","centro");
+  }
+}
+
+//
+// var namecoder= document.createElement("span");
+// imagen.setAttribute("src","assets/image/"+ i +".png");
+// namecoder.innerHTML=coder[i];
+// imagen.setAttribute("class","foto");
+// div.setAttribute("class","imagen");
+// namecoder.setAttribute("class","text");
+// div.appendChild(imagen);
+// div.appendChild(namecoder);
